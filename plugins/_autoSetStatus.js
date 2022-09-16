981d7db9e8e913f295a739e08e56edf6034af876
ཌ✼ʟᴇᴠɪ-ʏᴀɴ-xʜɪᴀᴏ✼ད <111663350+ImleviBotzz@users.noreply.github.com>
2022-09-17 05:19:53 +0800

    Update _autoSetStatus.js

--- plugins/_autoSetStatus.js
+++ plugins/_autoSetStatus.js
@@ -4,8 +4,8 @@ export async function all(m) {
 	if (new Date() * 1 - setting.status > 1000) {
 		let _uptime = process.uptime() * 1000
 		let uptime = clockString(_uptime);
-		let wm = '𝙳𝚁𝙰𝙶𝙾𝙽-𝙱𝙾𝚃𝚉-𝙼𝙳'
-		let bio = `🗓️Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.self ? 'Private' : global.opts['gconly'] ? 'Hanya Grup' : 'Publik'} | 🔥 Sc By ${wm}`
+		let wm = '𝙳𝚁𝙰𝙶𝙾𝙽-𝙱𝙾𝚃𝚉'
+		let bio = `⏰Aktif selama ${uptime} | Mode: ${global.opts['self'] ? 'Private' : setting.self ? 'Private' : global.opts['gconly'] ? 'Hanya Grup' : 'Publik'} | ⏳ Sc By ${wm}`
 
 		await this.setBio(bio).catch(_ => _)
 		setting.status = new Date() * 1
